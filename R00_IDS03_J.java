7d3af105ac75c4268af80c305bf96098c2d7e695
Matt Reid <mattreid@matts-mbp.lan>
2020-10-25 18:00:22 -0400

    added file

---
+++ R00_IDS03_J.java
@@ -0,0 +1,13 @@
+// Rule 00.  Input Validation and Data Sanitization (IDS)
+
+public class R00_IDS03_J {
+
+    public static void main(String[] args) {
+        if (loginSuccessful) {
+            logger.severe("User login succeeded for: " + sanitizeUser(username));
+          } else {
+            logger.severe("User login failed for: " + sanitizeUser(username));
+          }
+    }
+
+}
