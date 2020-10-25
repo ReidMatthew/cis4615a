162be7e7f6f49a9e4a138a91f491a91fa103f56f
Matt Reid <mattreid@matts-mbp.lan>
2020-10-25 18:48:53 -0400

    reverted R00 to origional

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -2,12 +2,12 @@
 
 public class R00_IDS03_J {
 
-    public static void main(String[] args) {
-        if (loginSuccessful) {
-            logger.severe("User login succeeded for: " + sanitizeUser(username));
-          } else {
-            logger.severe("User login failed for: " + sanitizeUser(username));
-          }
+  public static void main(String[] args) {
+    if (loginSuccessful) {
+      logger.severe("User login succeeded for: " + username);
+    } else {
+      logger.severe("User login failed for: " + username);
     }
+  }
 
 }
