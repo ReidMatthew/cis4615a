db741565c7cbaf800099f19105a03fef4dfece3f
Matt Reid <mattreid@matts-mbp.lan>
2020-10-25 18:30:58 -0400

    added files

---
+++ R02_XP00_J.java
@@ -0,0 +1,15 @@
+// Rule 02. Expressions (EXP)
+
+public class R02_XP00_J {
+
+    public static void main(String[] args) {    }
+
+    public void deleteFile(){
+ 
+        File someFile = new File("someFileName.txt");
+        // Do something with someFile
+        someFile.delete();
+       
+      }
+
+}
---
+++ R03_NUM03_J.java
@@ -0,0 +1,13 @@
+// Rule 03. Numeric Types and Operations (NUM)NUM03-J. 
+// Use integer types that can fully represent the possible 
+// range of unsigned dataGiven thenon-compliantcode below:
+
+public class R03_NUM03_J.  {
+
+    public static void main(String[] args) {}
+
+    public static int getInteger(DataInputStream is) throws IOException {
+        return is.readInt(); 
+      }
+
+}
